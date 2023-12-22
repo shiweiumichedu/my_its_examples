3dc9617dd4fdf4775647837ce0516d71ce98ac82
Ben Andries <110184975+benandries@users.noreply.github.com>
2023-12-22 12:03:52 -0500

    Updated with newer openai library

--- Python/python-langchain.py
+++ Python/python-langchain.py
@@ -1,17 +1,26 @@
 from langchain.schema import HumanMessage
 from langchain.chat_models import AzureChatOpenAI
-from dotenv import dotenv_values
+import os
+from dotenv import load_dotenv
+
+#Sets the current working directory to be the same as the file.
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 #Load environment file for secrets.
-secrets = dotenv_values(".env")  
+try:
+    if load_dotenv('.env') is False:
+        raise TypeError
+except TypeError:
+    print('Unable to load .env file.')
+    quit()
 
 #Define llm parameters
 llm = AzureChatOpenAI(
-    deployment_name=secrets['model'],
-    openai_api_version=secrets['API_VERSION'],
-    openai_api_key=secrets['OPENAI_API_KEY'],
-    openai_api_base=secrets['openai_api_base'],
-    openai_organization=secrets['OPENAI_organization']
+    deployment_name=os.environ['model'],
+    openai_api_version=os.environ['API_VERSION'],
+    openai_api_key=os.environ['OPENAI_API_KEY'],
+    azure_endpoint=os.environ['openai_api_base'],
+    openai_organization=os.environ['OPENAI_organization']
     )
 
 #Ask a query
