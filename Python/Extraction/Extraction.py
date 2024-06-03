7c9e03f803e186aa206db6b5307e3e90700a440c
Ben Andries <110184975+benandries@users.noreply.github.com>
2024-06-03 16:08:14 -0400

    Update python

--- Python/Extraction/Extraction.py
+++ Python/Extraction/Extraction.py
@@ -1,19 +1,16 @@
 from openai import AzureOpenAI
 import os
 from dotenv import load_dotenv
-from extract import labels,assisstant_message,system_message
 
-#Sets the current working directory to be the same as the file.
+# Sets the current working directory to be the same as the file.
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-#Load environment file for secrets.
-try:
-    if load_dotenv('.env') is False:
-        raise TypeError
-except TypeError:
+# Load environment file for secrets.
+if not load_dotenv('.env'):
     print('Unable to load .env file.')
     quit()
-#Create Azure client
+
+# Create Azure client
 client = AzureOpenAI(
     api_key=os.environ['OPENAI_API_KEY'],  
     api_version=os.environ['API_VERSION'],
@@ -21,21 +18,68 @@ client = AzureOpenAI(
     organization = os.environ['OPENAI_organization']
 )
 
-#Call labels function with 'pi' option (other options are orderform/resume)
-labels_list = labels('pi')
+# Returns a list of entities based on the label given.
+def labels():
 
-#Create usermessage function
+    # create a dictionary with your entity labels.  By defining which label goes with each entity, we can build a list based on label.
+    labels_list = {
+        'University',
+        'Department',
+        'Role',
+        "Supervisor Name",
+        "Facility",
+        "City",
+        "State",
+        "Location",
+        "Event",
+        "Work_of_art",
+        "Date",
+        "Time",
+        "Quantity",
+        "Mascot",
+    }
+
+    return labels_list
+
+# Creates the assistant message for the api call.  The assistant message gives an example of how the LLM should respond.
+def assistant_message():
+    return f"""
+EXAMPLE:
+    Text: 'In Germany, in 1440, goldsmith Johannes Gutenberg invented the movable-type printing press. His work led to an information revolution and the unprecedented mass-spread / 
+    of literature throughout Europe. Modelled on the design of the existing screw presses, a single Renaissance movable-type printing press could produce up to 3,600 pages per workday.'
+    {{
+        "gpe": ["Germany", "Europe"],
+        "date": ["1440"],
+        "person": ["Johannes Gutenberg"],
+        "product": ["movable-type printing press"],
+        "event": ["Renaissance"],
+        "quantity": ["3,600 pages"],
+        "time": ["workday"]
+    }}
+--"""
+
+def system_message(labels_list):
+    #print(type(labels_list))
+    types=", ".join(labels_list)
+    return f"""
+You are an expert in Natural Language Processing. Your task is to identify common Named Entities (NER) in a given text.
+The possible common Named Entities (NER) types are exclusively: ({types})."""
+
+# Call labels function with 'pi' option (other options are orderform/resume)
+labels_list = labels()
+
+# Create usermessage function
 def user_message(text):
     return f"""
 TASK:
     Text: {text}
 """
 
-#Call Azure API
+# Call Azure API
 def run_ner_task(labels_list, text):
     messages = [
           {"role": "system", "content": system_message(labels_list)},
-          {"role": "assistant", "content": assisstant_message()},
+          {"role": "assistant", "content": assistant_message()},
           {"role": "user", "content": user_message(text=text)}
       ]
 
@@ -50,15 +94,14 @@ def run_ner_task(labels_list, text):
     response_message = response.choices[0].message
     return response_message
 
-#Sample text
-text = """"I love my job supporting Dr. Maya Hammoud.
-At CVC5, I my co-worker, Harry Potter makes me proud to work at Michigan Medicine. 
-I love working at MichMed and really think my unit, PEDS Trauma, is a model for other units. 
-UofM is a wonderful place to work.
-Univ. Of M is the place to be. 
-My supervisor, Josiah K. Lee, is absolutely incredible. 
-I work over at the NCRC but I feel like Research is the best place under the guidance of Dr. Beneficient. 
-The psych dept. is a wonderful place to work."""
+# Sample text
+text = """"I am a student at the University of Michigan in Ann Arbor, MI.
+I like to go to football games at Michigan's football stadium, The Big House. The Big House's seating capacity is 107,601 people. 
+The first game of the 2024 football season is on August 31st. U of M's fight song is called The Victors.
+I work at the U-M ITS department in GenAI Services. 
+I have meetings with my supervisors, Ben and Don, each workday at 9:30 am.
+The Ann Arbor campus is divided into four main areas: North campus, Central campus, Medical campus, and South campus, for a combined area of more than 37.48 million square feet.
+UMich's mascot is the wolverine."""
 
 result = run_ner_task(labels_list, text)
 print(result.content)
--- Python/python-langchain.py
+++ Python/python-langchain.py
@@ -1,12 +1,11 @@
-from langchain.schema import HumanMessage
-from langchain.chat_models import AzureChatOpenAI
+from langchain_openai import AzureChatOpenAI
 import os
 from dotenv import load_dotenv
 
-#Sets the current working directory to be the same as the file.
+# Sets the current working directory to be the same as the file.
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-#Load environment file for secrets.
+# Load environment file for secrets.
 try:
     if load_dotenv('.env') is False:
         raise TypeError
@@ -14,7 +13,7 @@ except TypeError:
     print('Unable to load .env file.')
     quit()
 
-#Define llm parameters
+# Define llm parameters
 llm = AzureChatOpenAI(
     deployment_name=os.environ['model'],
     openai_api_version=os.environ['API_VERSION'],
@@ -23,9 +22,9 @@ llm = AzureChatOpenAI(
     openai_organization=os.environ['OPENAI_organization']
     )
 
-#Ask a query
-msg = HumanMessage(content="Explain step by step. Where is the University of Michigan?")
+# Ask a query
+prompt = "Explain step by step. Where is the University of Michigan?"
 
-#Get and print response
-response = llm(messages=[msg])
+# Get and print response
+response = llm.invoke(prompt)
 print(response.content)
