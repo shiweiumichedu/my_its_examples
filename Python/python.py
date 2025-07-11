53849cdc06cc6369459baeda97fea8e8f56e4e42
shiweiumichedu <shiwei@med.umich.edu>
2025-07-11 16:16:30 -0400

    updated python.py

--- Python/python.py
+++ Python/python.py
@@ -19,12 +19,16 @@ client = AzureOpenAI(
     api_version=os.environ['API_VERSION'],
     azure_endpoint = os.environ['OPENAI_API_BASE'],
     organization = os.environ['OPENAI_ORGANIZATION']
+#    api_key='aa13d8a1cb2c436db1d3fe4aadbf4fc9', 
+#    api_version='2024-05-01-preview',
+#    azure_endpoint = 'https://api.umgpt.umich.edu/azure-openai-api',
+#    organization = '229766'
 )
 
 #Create Query
 messages=[
         {"role": "system","content": "You are a helpful assistant.  Always say GO BLUE! at the end of your response."},
-        {"role": "user","content": "Explain step by step. Where is the University of Michigan?"},
+        {"role": "user","content": "Explain step by step. How a new roof is replaced."},
     ]
 
 # Send a completion request.
